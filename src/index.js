94f0528a398da04c508f486f6330917b683b3dec
Roohan <gmroohan@gmail.com>
2025-09-21 21:33:30 +0500

    util codes add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
 import connectToDatabase from "./database/index.js";
+import app from "./app.js";
+
 dotenv.config({
-    path: "./env"
+  path: "./env",
 });
 
-
-connectToDatabase();
+connectToDatabase()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.error("Server error:", error);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+  });
