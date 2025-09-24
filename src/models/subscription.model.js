9cbb0d72f629c8dba9d85a988a3bf0f1e6b858cf
Roohan <gmroohan@gmail.com>
2025-09-24 17:36:57 +0500

    feat: add user channel profile and watch history retrieval features

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const subscriptionSchema = new Schema(
   {
     subscriber: { type: Schema.Types.ObjectId, ref: "User" },
-    chanel: { type: Schema.Types.ObjectId, ref: "User" },
+    channel: { type: Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
